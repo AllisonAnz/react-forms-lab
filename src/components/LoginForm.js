39bd93555e0fbd3142553c16459b74999cd2ae6c
AllisonAnz <AllisonAnzalone@gmail.com>
2021-06-14 10:19:45 -0500

    Completed Assignment

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,27 +1,69 @@
 import React from "react";
 
 class LoginForm extends React.Component {
+  //takes one prop: handleLogin function: when called the form is being submitted 
+  //by default, the function just includes console.log (allows you to see the form function correctly when working in your browser)
+  //two input components <input type="text"> and <input type="password">, make 
+  //this a controlled component by adding in attributes to these inputs. 
+  //the input values should be saved to the component's state on every change 
+  //Ex:
+  //  <input 
+  //    id="username"
+  //    type="text"
+  //    name="username"
+  //    value={this.state.username}
+  //    onChange={this.handleInputChange}
+  //  />
+  //You can retrieve the input name and value of an event.target from the JS event 
+  //add the event handler to the <form> element in order to call the onSubmit callback prop 
+  //The onSubmit callback prop should only be called if both fields are filled (with any value)
+  
+  //Saving input value in state: save username and password in state when the input changes                      
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  //handleLogin callback prop 
+  //should call the prevent the default action when form is being submitted 
+  //should not call the 'handleLogin" callback prop when the user or password field is empty 
+  //should call the 'handleLogin' cb prop when the form is being submitted 
+  handleSubmit = event => {
+    event.preventDefault()
+    if (!this.state.username || !this.state.password) 
+      return 
+        this.props.handleLogin(this.state)
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text"
+                    onChange={this.handleChange} value={this.state.username} />
           </label>
         </div>
+
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" 
+                    onChange={this.handleChange} value={this.state.password}/>
           </label>
         </div>
+
         <div>
           <button type="submit">Log in</button>
         </div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,35 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
+  //Takes one prop: maxChars which is a number: the max amt of chars a message can have. This prop is passed in from the App comp w/value of 280
+  //Make <input type="text"> a controlled compontent by assing the attributes to the input el. 
+  //its value should be saved in the comp. state and update on every change to the input 
+  //Show the ramaining char. in the component. It doesn't matter how you render it, as long as the number is correct. 
   constructor() {
     super();
 
-    this.state = {};
+    //Saving input value in state
+    this.state = {
+      message: ''
+    };
   }
 
+    //handleChange: update the state when typing
+    handleChange = event => {
+      this.setState({
+        message: event.target.value
+      })
+    }
+
   render() {
+    //Character Counter: Show ramining character counter, should update the counter when typing 
+    let charNumber = this.props.maxChars - this.state.message.length
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message"
+                onChange={this.handleChange} value={this.state.message} />
+        {charNumber}
       </div>
     );
   }
